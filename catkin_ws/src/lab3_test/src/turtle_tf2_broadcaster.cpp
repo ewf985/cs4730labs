66b43e9b373181021937265bf4111cb020020434
Jacob <jakevoshage@gmail.com>
2020-09-30 20:13:33 -0500

    lab3 test files

---
+++ catkin_ws/src/lab3_test/src/turtle_tf2_broadcaster.cpp
@@ -0,0 +1,39 @@
+#include <ros/ros.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <turtlesim/Pose.h>
+
+
+void poseCallback(const turtlesim::PoseConstPtr& msg){
+  static tf2_ros::TransformBroadcaster br;
+  geometry_msgs::TransformStamped transformStamped;
+  
+  transformStamped.header.stamp = ros::Time::now();
+  transformStamped.header.frame_id = "world";
+  transformStamped.child_frame_id = "turtle1";
+  transformStamped.transform.translation.x = msg->x;
+  transformStamped.transform.translation.y = msg->y;
+  transformStamped.transform.translation.z = 0.0;
+  tf2::Quaternion q;
+  q.setRPY(0, 0, msg->theta);
+  transformStamped.transform.rotation.x = q.x();
+  transformStamped.transform.rotation.y = q.y();
+  transformStamped.transform.rotation.z = q.z();
+  transformStamped.transform.rotation.w = q.w();
+
+  br.sendTransform(transformStamped);
+}
+
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "my_tf2_broadcaster1");
+
+  ros::NodeHandle private_node("~");    
+  ros::NodeHandle node;
+  ros::Subscriber sub = node.subscribe("turtle1/pose", 10, &poseCallback);
+  target_location(4,9,90);
+
+  ros::spin();
+  return 0;
+};
